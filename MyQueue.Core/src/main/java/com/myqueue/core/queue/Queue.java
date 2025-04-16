757e50888a4550d663a7d2ac651b4d2251c37971
feketrob <robert-fekete@users.noreply.github.com>
2025-04-16 18:16:18 +0100

    Adding in-flight message store and ACK command

--- MyQueue.Core/src/main/java/com/myqueue/core/queue/Queue.java
+++ MyQueue.Core/src/main/java/com/myqueue/core/queue/Queue.java
@@ -2,11 +2,16 @@ package com.myqueue.core.queue;
 
 import com.myqueue.core.Message;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.LinkedList;
 
 public class Queue {
 
     private final java.util.Queue<MessageDelivery> queue = new LinkedList<>();
+    private final Map<Long, MessageDelivery> inFlightMessages = new HashMap<>();
+
+
 
     public void enqueue(Message message) {
         queue.add(new MessageDelivery(message));
@@ -17,6 +22,13 @@ public class Queue {
             return Message.empty();
         }
 
-        return queue.poll().getMessage();
+        final var delivery = queue.poll();
+        inFlightMessages.put(delivery.getMessageId(), delivery);
+
+        return delivery.getMessage();
+    }
+
+    public void commit(long messageId){
+        inFlightMessages.remove(messageId);
     }
 }
