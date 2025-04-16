e219733f80cf1d753c75b07a11166e0ac6cb89e6
feketrob <robert-fekete@users.noreply.github.com>
2025-04-16 14:50:21 +0100

    Fixing warnings

--- MyQueue.Core/src/main/java/com/myqueue/core/configuration/QueueConfig.java
+++ MyQueue.Core/src/main/java/com/myqueue/core/configuration/QueueConfig.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class QueueConfig {
 
-    private List<String> queues = List.of("test");
+    private final List<String> queues = List.of("test");
 
     public List<String> getQueues() {
         return queues;
